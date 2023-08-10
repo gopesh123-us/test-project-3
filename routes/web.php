419005ccf7f450a2f23ed556b3a61a871fedfe34
Gopesh Sharma <gopesh.sharma@gmail.com>
2023-08-10 14:48:29 -0400

    fallback view

--- routes/web.php
+++ routes/web.php
@@ -28,32 +28,32 @@ Route::get('/myhome', [MyHomeController::class, 'index'])->name('my.home');
 
 Route::get('/home', function () {
     $blogs =
-    [
         [
-            'title' => 'Title One',
-            'body'=> 'Title One body. Lorem ipsum dolor sit amet consec',
-            'status' => 1
+            [
+                'title' => 'Title One',
+                'body' => 'Title One body. Lorem ipsum dolor sit amet consec',
+                'status' => 1
 
-        ],
-        [
-            'title' => 'Title two',
-            'body'=> 'Title two body. Lorem ipsum dolor sit amet consec',
-            'status' => 1
+            ],
+            [
+                'title' => 'Title two',
+                'body' => 'Title two body. Lorem ipsum dolor sit amet consec',
+                'status' => 1
 
-        ],
-        [
-            'title' => 'Title three',
-            'body'=> 'Title three body. Lorem ipsum dolor sit amet consec',
-            'status' => 0
+            ],
+            [
+                'title' => 'Title three',
+                'body' => 'Title three body. Lorem ipsum dolor sit amet consec',
+                'status' => 0
 
-        ],
-        [
-            'title' => 'Title four',
-            'body'=> 'Title four body. Lorem ipsum dolor sit amet consec',
-            'status' => 1
+            ],
+            [
+                'title' => 'Title four',
+                'body' => 'Title four body. Lorem ipsum dolor sit amet consec',
+                'status' => 1
 
-        ]
-    ];
+            ]
+        ];
     return view('home', compact('blogs'));
 });
 
@@ -78,4 +78,7 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 //CSRF TOKEN
 Route::post('/login', [LoginController::class, 'handleLogin'])->name('login.submit');
 
-
+Route::fallback(function () {
+    $message = 'Please check the URL. This page does not exit';
+    return view('/fallback', ['msg' => $message]);
+});
